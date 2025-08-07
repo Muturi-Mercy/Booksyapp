df5da736018ee3c038ea27aea3539f27c91a132c
Muturi-Mercy <muturi.n.mercy@gmail.com>
2025-08-07 16:20:56 +0300

    dockerchanges

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //  default admin account
+        User::create([
+            'name' => 'Admin',
+            'email' => 'adminbooksy@gmail.com',
+            'password' => Hash::make('booksycrud'), 
+            'role' => 'admin',
         ]);
+
+        
+
+        echo "Admin account created:\n";
+        echo "Email: adminbooksy@gmail.com\n";
+        echo "Password: booksycrud\n";
     }
 }
